40d861311d85a5889e121d489eda1c22ee2c0268
I-NOZex <tiagofm.profissional@outlook.pt>
2022-02-16 15:41:52 +0100

    storybook + vue setup

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    framework: '@storybook/vue3',
+
+      webpackFinal: async (config) => ({
+        ...config,
+        resolve: {
+          ...config.resolve,
+          alias: {
+            ...config.resolve?.alias,
+            '@': path.resolve(__dirname, '../src/'),
+            '@components': path.resolve(__dirname, '../src/stories/'),
+          },
+        },
+      }),
+};
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>Zendesk Garden - Vue Components based on Garden Css Components</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 });
 </script>
 
