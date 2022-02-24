a888604ad464f3516ab9f9a3b45e490d63529f7e
I-NOZex <tiagofm.profissional@outlook.pt>
2022-02-24 16:48:37 +0100

    sass support

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,18 +5,47 @@ module.exports = {
         '../src/**/*.stories.mdx',
         '../src/**/*.stories.@(js|jsx|ts|tsx)'
     ],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-postcss'
+    ],
     framework: '@storybook/vue3',
 
-      webpackFinal: async (config) => ({
+    webpackFinal: async (config) => ({
         ...config,
-        resolve: {
-          ...config.resolve,
-          alias: {
-            ...config.resolve?.alias,
-            '@': path.resolve(__dirname, '../src/'),
-            '@components': path.resolve(__dirname, '../src/stories/'),
-          },
+
+        module: {
+            rules: [
+                ...config?.module?.rules,
+                {
+                    test: /\.s(a|c)ss$/,
+                    include: path.resolve(__dirname, '../'),
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'vue-style-loader',
+                        // Translates CSS into CommonJS
+                        {
+                            loader: 'css-loader',
+                            options: { importLoaders: 1 }
+                        },
+                        'postcss-loader', 
+                        // Compiles Sass to CSS
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                }
+            ]
         },
-      }),
+
+        resolve: {
+            ...config.resolve,
+            alias: {
+                ...config.resolve?.alias,
+                '@': path.resolve(__dirname, '../src/'),
+                '@stories': path.resolve(__dirname, '../src/stories/')
+            }
+        }
+    })
 };
